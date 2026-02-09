fac17b23032ae6bfff310be3f6bc3bf70c1e3bcb
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-09 21:17:30 +0530

    switch case

---
+++ switchcase.js
@@ -0,0 +1,22 @@
+
+let browser = '   CHrome';
+
+switch(browser.toLowerCase().trim())
+{
+    case 'chrome': console.log('chrome browser');
+                    break;
+
+    case 'firefox': console.log('firefox browser');
+                    break;
+
+    case 'edge': console.log('edge browser');
+                    break;
+
+    case 'safari': console.log('safari browser');
+                    break;
+
+    default: console.log('Invalid browser.....please enter the correct browser...');
+                    break;
+
+
+}
