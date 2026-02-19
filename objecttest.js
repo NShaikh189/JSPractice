12763c8dfd2d5abbf6b441edfa3a4c7340cc8857
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-19 23:33:32 +0530

    object test

---
+++ objecttest.js
@@ -0,0 +1,27 @@
+let u1 ={
+    name: 'ravi',
+    age: 30,
+    city: 'USA'
+};
+
+let u2 ={
+    name: 'amit',
+    age: 34,
+    city: 'Pune',
+    country: 'India'
+}
+
+// console.log(u1.country);
+// u1= u2;
+// console.log(u1.name);
+// console.log(u1.age);
+// console.log(u1.country);
+// console.log(typeof u1);//object
+
+let u3 = {};
+console.log(u3);
+
+u1 = u3;
+console.log(u1);
+console.log(u2);
+console.log(u3);
