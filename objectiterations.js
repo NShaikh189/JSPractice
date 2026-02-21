e98f9e01e0cf97337e83706012b3a408fc242c7c
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-21 13:24:22 +0530

    object iterations

--- objectiterations.js
+++ objectiterations.js
@@ -3,11 +3,22 @@ let user =
     name: 'Tom',
     age : 30,
     city: 'LA',
-    isActive: true
+    isActive: true,
+    address: {
+        flat: 101,
+        zip: 411017,
+        lat: 10.34,
+        long: 11.22
+    },
+    devices: ['iphone','mouse','keyboard']
 }
 
 
 for(let key in user)
 {
-    console.log(key, '===>', user[key]);
+    if(key === 'address')
+    {
+     //   console.log(key, '===>', user[key]);
+        console.log(user[key]['flat']);
+    }
 }
