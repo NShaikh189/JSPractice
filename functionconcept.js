02f6ebdddd1ea4717540dcdc476fb1f9690495ae
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-24 23:32:14 +0530

    function concept

--- functionconcept.js
+++ functionconcept.js
@@ -16,6 +16,8 @@ function testLogin()
 
 test();
 
+
+//2. no input and return
 function getName()
 {
     return "Nayum";
@@ -23,3 +25,32 @@ function getName()
 
 let name = getName();
 console.log(name);
+
+//3. some input and return
+function getStatus(name)
+{
+    if(name === 'nayum')
+    return true;
+else
+    return false;
+}
+
+let status = getStatus('ajay');
+console.log(status);
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
+//4. some input and no return
+function add(a,b)
+{
+    let result = a + b;
+    console.log(result);
+}
+
+add(5,6); //11
+add('James',20); //James
+add(10,'20');//1020
+add(10,+'20');//30
