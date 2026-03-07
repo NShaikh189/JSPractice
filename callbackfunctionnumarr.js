908f2c1f04e757d0e0d9b3e0aabeeab157d503ea
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-07 15:51:41 +0530

    call back function num array

---
+++ callbackfunctionnumarr.js
@@ -0,0 +1,7 @@
+
+
+let num = [1,2,3,4,5];
+
+//method) Array<number>
+// .forEach(callbackfn: (value: number, index: number, array: number[]) => void, thisArg?: any): void
+num.forEach(e=> console.log(e));
