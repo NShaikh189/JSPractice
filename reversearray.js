4d950ccc52dc0aa6def862ed9f14dd3b0dc36caf
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-18 23:13:56 +0530

    reversearray

--- reversearray.js
+++ reversearray.js
@@ -1,5 +1,5 @@
 
-let num = [1,2,3,4,5];
+let num = [1,2,31,4,5];
 //let cnt = num.length-1;
 
 for(let e of num)
@@ -14,4 +14,14 @@ for(let e in num)
     let count = num.length-e -1 ;
     console.log(num[count]);
 }
+console.log('--------------');
+for(let i of num)
+{
 
+    let index = num.length-1-num.indexOf(i);
+    console.log(num[index]);
+}
+
+console.log('--------------');
+let nums = num.map(n=> num[num.length-1 - num.indexOf(n)]);
+console.log(nums);
