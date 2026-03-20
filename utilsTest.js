5be90c4ed23ce4191e7c7d653918f243bee83947
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-20 23:02:48 +0530

    utils test

---
+++ utilsTest.js
@@ -0,0 +1,18 @@
+
+//import print, {name, add} from './utils.js';
+//import printInfo, {name as empName, add as sum} from './utils.js';
+
+import * as utils from './utils.js';
+
+// console.log(name);
+// console.log(add(42,10));
+// console.log(empName);
+// console.log(sum(22,10));
+console.log(utils.name);
+console.log(utils.add(42,10));
+// import{test} from './utils.js';
+// test();
+
+// printInfo();
+utils.test();
+utils.default();
