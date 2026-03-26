02b7f3e49d387573bad874183e1a435ab740d30d
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-26 23:14:12 +0530

    typescript file

---
+++ app.ts
@@ -0,0 +1,15 @@
+//app.ts => (compiler - tsc) => app.js (Browser only understands Javascript) => node JS server => browser
+
+// let x = 10;
+// x ='nayum'; //Type 'string' is not assignable to type 'number'.
+// console.log(x);
+
+
+// function Name(stName: string)
+// {
+//     return stName.toLowerCase();
+// }
+
+console.log('chrome');
+//console.log(Name("CHROME"));
+//console.log(myName(100));
